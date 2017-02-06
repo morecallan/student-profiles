270fbbc642863b73155e3ba1f169184baa080b30
Callan Morrison <callan.l.morrison@gmail.com>
2017-02-05 20:28:59 -0600

    updated file structure for hosting

---
+++ public/app/data/firebase.config.js
@@ -0,0 +1,7 @@
+app.constant("FIREBASE_CONFIG", {
+  apiKey: "AIzaSyCHtSMeKyrUxz-Rs2T73_BqY0opzVIiJ48",
+  authDomain: "student-profiles-3afb4.firebaseapp.com",
+  databaseURL: "https://student-profiles-3afb4.firebaseio.com",
+  storageBucket: "student-profiles-3afb4.appspot.com",
+  messagingSenderId: "1075968140791"
+});
