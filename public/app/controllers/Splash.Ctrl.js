82ad29bd8839f685136987a8e2323d76ac751784
Callan Morrison <callan.l.morrison@gmail.com>
2017-02-25 13:28:33 -0600

    modal open and close now working

--- public/app/controllers/Splash.Ctrl.js
+++ public/app/controllers/Splash.Ctrl.js
@@ -1,16 +1,27 @@
 app.controller("SplashCtrl", function($scope, StudentFactory){
   StudentFactory.returnAllStudents().then((data) => {
-    console.log(data)
     $scope.students = data
     for (var i = 0; i < $scope.students.length; i++) {
-      $scope.students[i].statusColor = colorBasedOnStatus($scope.students[i]);
+      $scope.students[i].statusColor = colorBasedOnStatus($scope.students[i].status);
     }
   })
 
-  // $scope.modalOpen = true;
+  $scope.modalOpen = false;
+  $scope.editModalOpen = false;
 
   $scope.modalShift = (context) => {
     context.modalOpen = !context.modalOpen;
+  }
+
+  $scope.editModalShift = (context) => {
+    context.editModalOpen = !context.editModalOpen;
+  }
+
+  $scope.closeAllModals = (context, event) => {
+    if ((context.editModalOpen || context.modalOpen) && event.target.className.indexOf("material-icons") == -1) {
+      context.editModalOpen = false;
+      context.modalOpen = false;
+    }
   }
 
   $scope.deleteStudent = (studentId) => {
@@ -19,7 +30,17 @@ app.controller("SplashCtrl", function($scope, StudentFactory){
     })
   }
 
+  $scope.editStudentStatus = (studentId, status) => {
+    console.log(studentId, status)
+    // StudentFactory.editStudentStatus(studentId, status).then((data) => {
+    //   $scope.students = data
+    // })
+  }
+
+
+
   const colorBasedOnStatus = (status) => {
+    console.log(status)
     if (status == "enrolled") {
       return "yellow"
     } else if (status == "seeking") {
