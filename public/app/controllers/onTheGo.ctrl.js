6c73187f147abbac526f14c477d257ad4573dd17
Callan Morrison <callan.l.morrison@gmail.com>
2017-03-21 23:06:53 -0500

    update dropdown population for landing

--- public/app/controllers/onTheGo.ctrl.js
+++ public/app/controllers/onTheGo.ctrl.js
@@ -1,3 +1,37 @@
-app.controller("OnTheGoCtrl", function($scope){
-  console.log("my on the go");
+app.controller("OnTheGoCtrl", function($scope, DataFactory){
+  var populateDropdownJobs = () => {
+    DataFactory.returnPreviousExperienceList().then((data)=> {
+      $(document).ready(function() {
+        $('select').material_select();
+      });
+      $scope.previous = data;
+    })
+  }
+
+  var populateDropdownDev = () => {
+    DataFactory.returnTypeOfDevList().then((data)=> {
+      $(document).ready(function() {
+        $('select').material_select();
+      });
+      $scope.devTypes = data;
+    })
+  }
+
+  var populateDropdownCompany = () => {
+    DataFactory.returnTypeOfCompanyList().then((data)=> {
+      $(document).ready(function() {
+        $('select').material_select();
+      });
+      $scope.companyTypes = data;
+    })
+  }
+
+  var populateAreaOfInterest = () => {
+    DataFactory.returnAreasOfInterest().then((data)=> {
+      $(document).ready(function() {
+        $('select').material_select();
+      });
+      $scope.areaOfInterests = data;
+    })
+  }
 })
